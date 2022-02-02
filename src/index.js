af07073140575f786306f89b8d2e07054f725bcd
taylor-ann <80003984+taylor-ann@users.noreply.github.com>
2022-02-02 17:01:51 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app">
+            <form id="search-form" className="mb-3">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="City..."
+                    className="form-control"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    class="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="overview">
+              <h1 id="city"></h1>
+              <ul>
+                <li>
+                  <span id="date"></span>
+                </li>
+                <li id="description"></li>
+              </ul>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="clearfix weather-temperature">
+                  <img src="" alt="Clear" id="icon" class="float-left" />
+                  <div className="float-left">
+                    <strong id="temperature"></strong>
+                    <span className="units">°C</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: <span id="humidity"></span>%
+                  </li>
+                  <li>
+                    Wind: <span id="wind"></span> m/h
+                  </li>
+                </ul>
+              </div>
+              <div className="weather-forcast" id="forecast"></div>
+            </div>
+          </div>
+          <small>
+            <a href="https://github.com/taylor-ann/Weather-App" target="_blank">
+              Open-source code
+            </a>
+            by Taylor Mattscheck
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
